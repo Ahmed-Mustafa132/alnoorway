438294404c492f9e8666ddaedfb4230aadec54ce
Ahmed-Mustafa132 <ahmedmustafa0384@gmail.com>
2026-04-16 17:46:58 +0200

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.osama.alnoorway",
+  appId: "com.alnoorway.app",
   appName: "Alnoorway",
   webDir: "dist",
   server: {
