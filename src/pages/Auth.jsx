5f209665373dfe7c65666ef277df7ff082ec9f08
Ahmed-Mustafa132 <ahmedmustafa0384@gmail.com>
2026-05-04 00:01:20 +0300

    fix

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -24,7 +24,7 @@ export default function AuthPage() {
     if (Capacitor.isNativePlatform()) {
       GoogleAuth.initialize({
         clientId: '829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte.apps.googleusercontent.com',
-        iosClientId: 'com.googleusercontent.apps.829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte', // أضف هذا السطر تحديداً للـ iOS
+        iosClientId: 'com.googleusercontent.apps.827572291550-8oqmpspdhbo71qn2frbgl55u2e6srqci', // أضف هذا السطر تحديداً للـ iOS
         scopes: ['profile', 'email'],
         grantOfflineAccess: true,
       });
