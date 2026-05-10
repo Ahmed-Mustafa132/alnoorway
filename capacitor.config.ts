52e76d7d4243635d08756d6ccc206b3cdc6122a1
Ahmed-Mustafa132 <ahmedmustafa0384@gmail.com>
2026-05-10 21:19:56 +0300

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,7 +13,7 @@ const config: CapacitorConfig = {
       scopes: ["profile", "email"],
       // الـ Client IDs دي بتجيبها من Google Cloud Console
       iosClientId:
-        "829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte.apps.googleusercontent.com",
+        "apps.googleusercontent.com.829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte",
       serverClientId:
         "829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte.apps.googleusercontent.com",
       forceCodeForRefreshToken: true,
