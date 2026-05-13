96d91d88fcf9dcbb6255afa2546da640733427d9
Ahmed-Mustafa132 <ahmedmustafa0384@gmail.com>
2026-05-13 17:37:57 +0300

    try  fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,15 +9,15 @@ const config: CapacitorConfig = {
     iosScheme: "com.alnoorway.app", // خليه نفس الـ App ID عشان الـ Deep Linking
   },
   plugins: {
-    GoogleAuth: {
-      scopes: ["profile", "email"],
-      // الـ Client IDs دي بتجيبها من Google Cloud Console
-      iosClientId:
-        "com.googleusercontent.apps.829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte",
-      serverClientId:
-        "829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte.apps.googleusercontent.com",
-      forceCodeForRefreshToken: true,
-    },
+    // GoogleAuth: {
+    //   scopes: ["profile", "email"],
+    //   // الـ Client IDs دي بتجيبها من Google Cloud Console
+    //   iosClientId:
+    //     "com.googleusercontent.apps.829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte",
+    //   serverClientId:
+    //     "829658324868-lrbdqm9ekjpaunpaecm4bk4stn16ifte.apps.googleusercontent.com",
+    //   forceCodeForRefreshToken: true,
+    // },
     Assets: {
       inputs: {
         icon: "resources/icon.png", // تأكد من وجود الصورة هنا
